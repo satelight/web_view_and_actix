3afd5f31a259d6c93b0be3a1c17dfff02d6bf934
satelightccb <crazy_soci80@yahoo.co.jp>
2023-09-17 22:33:56 +0900

    ファイルを追加

---
+++ src/service.rs
@@ -0,0 +1,26 @@
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!dfandgdan")
+}
+
+#[post("/echo")]
+async fn echo(req_body: String) -> impl Responder {
+    HttpResponse::Ok().body(req_body)
+}
+
+async fn manual_hello() -> impl Responder {
+    HttpResponse::Ok().body("Hey there!")
+}
+
+pub async fn run_server() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(hello)
+            .service(echo)
+            .route("/hey", web::get().to(manual_hello))
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
+}
